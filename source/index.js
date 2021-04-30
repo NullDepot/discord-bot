8444ea17e10b18a578ec8be0f6243edf84c3884b
Nathan Allison <nathan@arch-linux.localdomain>
2021-04-30 14:35:56 +0400

    Framework for reaction roles, ammended miscellaneous items in index.

--- source/index.js
+++ source/index.js
@@ -4,6 +4,8 @@ require('dotenv').config();
 const fs = require('fs');
 const Discord = require('discord.js');
 
+const roleClaim = require('./role-claim')
+
 const Keyv = require('keyv');
 const keyv = new Keyv(); // for in-memory storage
 keyv.on('error', err => console.error('Keyv connection error:', err));
@@ -19,6 +21,8 @@ client.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('commands').filter(file => file.endsWith('.js'));
 
+console.log('Bot initialising. Please wait...')
+
 for (const file of commandFiles) {
 	console.log('Loading.. '+file);
 	const command = require(`../commands/${file}`);
@@ -28,7 +32,15 @@ for (const file of commandFiles) {
 //Console notes once the bot is ready.
 client.once('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
+
+	// roleClaim(client)
 });
+client.once('reconnecting', () => {
+	console.log('Reconnecting...');
+   });
+client.once('disconnect', () => {
+	console.log('Disconnected.');
+   });
 
 //COMMANDS
 client.on('message', message => {
@@ -47,7 +59,31 @@ client.on('message', message => {
 	}
 });
 
-/*
+/* Bot welcomes new members.
+client.on('guildMemberAdd', member => {
+	const channel = message.guild.channels.cache.find(c => c.id === '837471968345063435')
+    const channelwelcomeEmbed = new Discord.MessageEmbed()
+        .setColor('#b80f0a')
+        .setTitle('Welcome to Barovia...')
+        .setDescription(`${member}, Your new home.`)
+        .setTimestamp();
+    channel.send(channelwelcomeEmbed);
+    const dmwelcomeEmbed = new Discord.MessageEmbed()
+        .setColor('#ffd6d6')
+        .setTitle('Welcome!')
+        .setDescription("For Help Using @Pro Bot#7903, Send The Command `!help` In Server")
+        .setTimestamp();
+    member.send(dmwelcomeEmbed);
+    let role6 = message.guild.roles.cache.find(role => role.name == 'Curse of Strahd'); //BASIC ROLE, EVERYONE GETS IT
+    if(!role6) return message.reply("Couldn't find that Role .")
+    member.roles.add(role6);
+}); */
+
+//	client.channels.cache.get(837471968345063435).send('**Welcome to Barovia, ' + member.user.username + '. Your new home...**')
+//	});
+//	channel.send(`Welcome to Barovia my new friends, your new home, ${member}`);
+//  });
+
 //Bot responds to hello and goodbye messages.
 client.on('message', (message) => {
   if (message.author.bot) return;
@@ -59,14 +95,15 @@ client.on('message', (message) => {
   }
 })
 
-//Bot replies with a 1/100 chance.
+//Bot replies with a 1/250 chance.
 client.on('message', (message) => {
+	if (message.author.bot) return;
 	if (message.content.startsWith(prefix)) return;
 	//Random number.
-	var ranNumb = Math.floor(Math.random() * 100);
+	var ranNumb = Math.floor(Math.random() * 250);
 	if (message.author.bot) return;
-	console.log(ranNumb);
-	if (ranNumb === 99) {
+	console.log('Random number = '+ ranNumb);
+	if (ranNumb === 0) {
 		message.reply('silence, wench.')
 	}
 })
@@ -74,17 +111,19 @@ client.on('message', (message) => {
 //Bot sends .gif or emoji when prompted by messages.
 client.on('message', (message) => {
 	if (message.author.bot) return;
-	if (message.content.toLowerCase().includes('yare')) {
+/*	if (message.content.toLowerCase().includes('yare')) {
     message.channel.send(`https://media1.tenor.com/images/162e6417f80df31ff2fcf72680f0424c/tenor.gif?itemid=13569904`)
 	} else if (message.content.toLowerCase().includes('dnd')) {
 		message.channel.send(`https://media1.tenor.com/images/e4641e64908840c937211e4b3e1d1406/tenor.gif?itemid=15179548`)
-	} else if (message.content.toLowerCase().includes('poggers')) {
-		message.channel.send(`<:poggers:816391730323783691>`)
+	} else*/ if (message.content.toLowerCase().includes('poggers')) {
+		message.react('<:poggers:816391730323783691>')
 	} else if (message.content.toLowerCase().includes('uwu')) {
 		message.channel.send(`UwU u so warm snuggie wuggies!!1!`)
+	} else if (message.content.toLowerCase().includes('irp')) {
+		message.react('<:irp:834156842551607327>')
 	}
 })
-*/
 
+  
 //Bot logs in with the token from .env
 client.login(process.env.BOT_TOKEN);
